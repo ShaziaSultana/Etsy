28bc783f378c161f9cb4512a12cbb966ec9a064e
ShaziaSultana <jita.shazia@gmail.com>
2022-08-25 20:19:26 -0400

    boiler plate

---
+++ src/main/java/com/etsy/AmazonApp.java
@@ -0,0 +1,10 @@
+package com.etsy;
+
+public class AmazonApp {
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+
+	}
+
+}
---
+++ src/test/java/com/etsy/pages/LoginPage.java
@@ -0,0 +1,5 @@
+package com.etsy.pages;
+
+public class LoginPage {
+
+}
